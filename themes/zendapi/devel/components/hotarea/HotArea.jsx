8676655c802d459e4359106ca52bf2f9cf19cd13
softboy <zzu_softboy@163.com>
2017-10-19 18:50:51 +0800

    翻译首页顶部

--- themes/zendapi/devel/components/hotarea/HotArea.jsx
+++ themes/zendapi/devel/components/hotarea/HotArea.jsx
@@ -13,13 +13,13 @@ export default class HotArea extends React.Component
          </div>
          <div className="btn uk-grid-small uk-child-width-expand@s" data-uk-grid>
             <div className="uk-width-1-1 uk-width-1-3@s uk-text-center uk-text-left@s">
-               <button onClick={this.gotoManual} className="uk-button uk-button-default uk-margin-medium-right@s uk-box-shadow-hover-large start-btn">开始学习</button>
+               <button onClick={this.gotoManual} className="uk-button uk-button-default uk-margin-medium-right@s uk-box-shadow-hover-large start-btn">Get Started</button>
             </div>
             <div className="uk-width-1-1 uk-width-1-3@s uk-text-center uk-text-left@s">
-               <button className="uk-button uk-button-default github-repo-btn" onClick={this.gotoGitee}>码云官方库</button>
+               <button className="uk-button uk-button-default github-repo-btn" onClick={this.gotoGitee}>Gitee Repo</button>
             </div>
             <div className="uk-width-1-1 uk-width-1-3@s uk-text-center uk-text-left@s">
-               <button className="uk-button uk-button-default github-repo-btn" onClick={this.gotoGithub}>GitHub 仓库</button>
+               <button className="uk-button uk-button-default github-repo-btn" onClick={this.gotoGithub}>GitHub Repo</button>
             </div>
          </div>
       </div>;
